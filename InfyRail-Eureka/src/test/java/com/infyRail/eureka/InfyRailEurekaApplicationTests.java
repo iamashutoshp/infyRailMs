519e0903fe67439d98a827fdb79fe6f0230c0bc0
iamashutoshp <ap1853177@gmail.com>
2021-09-21 02:33:31 +0530

    added eureka service

---
+++ InfyRail-Eureka/src/test/java/com/infyRail/eureka/InfyRailEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infyRail.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfyRailEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
